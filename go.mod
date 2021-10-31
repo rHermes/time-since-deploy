06347acb62da10a82e05d2dee3b5903a24b0be25
Teodor Spæren <teodor@sparen.no>
2021-10-31 20:35:35 +0100

    Add further HTTP tracing

--- go.mod
+++ go.mod
@@ -5,15 +5,25 @@ go 1.17
 require (
 	github.com/hako/durafmt v0.0.0-20210608085754-5c1018a4e16b
 	github.com/xanzy/go-gitlab v0.51.1
+	go.opentelemetry.io/contrib/instrumentation/net/http/httptrace/otelhttptrace v0.26.0
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.26.0
+	go.opentelemetry.io/otel v1.1.0
+	go.opentelemetry.io/otel/exporters/jaeger v1.1.0
+	go.opentelemetry.io/otel/sdk v1.1.0
 )
 
 require (
+	github.com/felixge/httpsnoop v1.0.2 // indirect
 	github.com/golang/protobuf v1.2.0 // indirect
 	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
 	github.com/hashicorp/go-retryablehttp v0.6.8 // indirect
+	go.opentelemetry.io/otel/internal/metric v0.24.0 // indirect
+	go.opentelemetry.io/otel/metric v0.24.0 // indirect
+	go.opentelemetry.io/otel/trace v1.1.0 // indirect
 	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
 	golang.org/x/oauth2 v0.0.0-20181106182150-f42d05182288 // indirect
+	golang.org/x/sys v0.0.0-20211025201205-69cdffdb9359 // indirect
 	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
 	google.golang.org/appengine v1.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -21,6 +21,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
+	"net/http/httptrace"
 	"os"
 	"runtime/trace"
 	"strings"
@@ -29,7 +31,17 @@ import (
 
 	"github.com/hako/durafmt"
 	"github.com/xanzy/go-gitlab"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/httptrace/otelhttptrace"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/sdk/resource"
+	tracesdk "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
 )
+
+const ONAME = "time-since-deploy"
 
 type EnvDep struct {
 	Name string
@@ -40,6 +52,36 @@ var (
 	projectFlag = flag.String("project", "", "selects the project to be used")
 	traceFlag   = flag.String("trace", "", "file to write trace to")
 )
+
+// tracerProvider returns an OpenTelemetry TracerProvider configured to use
+// the Jaeger exporter that will send spans to the provided url. The returned
+// TracerProvider will also use a Resource configured with all the information
+// about the application.
+func tracerProvider() (*tracesdk.TracerProvider, error) {
+	// Create the Jaeger exporter
+	exp, err := jaeger.New(jaeger.WithAgentEndpoint())
+	if err != nil {
+		return nil, err
+	}
+
+	// res, err := resource.New(context.Background(), resource.WithProcess(), resource.WithAttributes(semconv.ServiceNameKey.String(ONAME)))
+	// if err != nil {
+	// 	return nil, err
+	// }
+	res := resource.NewWithAttributes(
+		semconv.SchemaURL,
+		semconv.ServiceNameKey.String(ONAME),
+		attribute.String("environment", "dev"),
+		attribute.Int64("ID", 1),
+	)
+	tp := tracesdk.NewTracerProvider(
+		// Always be sure to batch in production.
+		tracesdk.WithBatcher(exp),
+		// Record information about this application in an Resource.
+		tracesdk.WithResource(res),
+	)
+	return tp, nil
+}
 
 func main() {
 	flag.Parse()
@@ -65,12 +107,39 @@ func main() {
 	ctx, tsk := trace.NewTask(ctx, "full-run")
 	defer tsk.End()
 
+	tp, err := tracerProvider()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Register our TracerProvider as the global so any imported
+	// instrumentation in the future will default to using it.
+	otel.SetTracerProvider(tp)
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	// Cleanly shutdown and flush telemetry when the application exits.
+	defer func(ctx context.Context) {
+		// Do not make the application hang when it is shutdown.
+		ctx, cancel = context.WithTimeout(ctx, time.Second*5)
+		defer cancel()
+		if err := tp.Shutdown(ctx); err != nil {
+			log.Fatal(err)
+		}
+	}(ctx)
+
+	ctx, span := otel.Tracer(ONAME).Start(ctx, "main")
+	defer span.End()
+
 	token, ok := os.LookupEnv("GITLAB_TOKEN")
 	if !ok {
 		log.Fatal("token not set\n")
 	}
 
-	c, err := gitlab.NewClient(token)
+	cli := http.Client{Transport: otelhttp.NewTransport(http.DefaultTransport)}
+
+	c, err := gitlab.NewClient(token, gitlab.WithHTTPClient(&cli))
 	if err != nil {
 		log.Fatalf("creating client: %v", err)
 	}
@@ -93,6 +162,9 @@ func main() {
 func getDrifts(ctx context.Context, c *gitlab.Client, pid int, envDeps []EnvDep) error {
 	ctx, tsk := trace.NewTask(ctx, "get-drifts")
 	defer tsk.End()
+
+	ctx, span := otel.Tracer(ONAME).Start(ctx, "get-drifts")
+	defer span.End()
 
 	var wg sync.WaitGroup
 
@@ -118,8 +190,15 @@ func getDrift(ctx context.Context, c *gitlab.Client, pid int, ed EnvDep) error {
 	ctx, tsk := trace.NewTask(ctx, "get-drift")
 	defer tsk.End()
 
+	ctx, span := otel.Tracer(ONAME).Start(ctx, "get-drift")
+	defer span.End()
+
+	span.SetAttributes(attribute.String("service", ed.Name))
+
+	ctx = httptrace.WithClientTrace(ctx, otelhttptrace.NewClientTrace(ctx))
+
 	trace.Log(ctx, "service", ed.Name)
-	penv, r, err := c.Environments.GetEnvironment(pid, ed.Prod)
+	penv, r, err := c.Environments.GetEnvironment(pid, ed.Prod, gitlab.WithContext(ctx))
 	if err != nil {
 		return fmt.Errorf("get prod environment: %v", err)
 	}
@@ -142,12 +221,16 @@ func getEnvs(ctx context.Context, c *gitlab.Client, pid int) ([]EnvDep, error) {
 	ctx, tsk := trace.NewTask(ctx, "get-envs")
 	defer tsk.End()
 
+	ctx, span := otel.Tracer(ONAME).Start(ctx, "get-envs")
+	defer span.End()
+
 	page := 1
 	perPage := 20
 
 	allEnvs := make([]*gitlab.Environment, 0)
 
 	for page != 0 {
+		ctx := httptrace.WithClientTrace(ctx, otelhttptrace.NewClientTrace(ctx))
 		envs, r, err := c.Environments.ListEnvironments(pid, &gitlab.ListEnvironmentsOptions{
 			ListOptions: gitlab.ListOptions{
 				Page:    page,
@@ -155,7 +238,7 @@ func getEnvs(ctx context.Context, c *gitlab.Client, pid int) ([]EnvDep, error) {
 			},
 			States: gitlab.String("available"),
 			Search: gitlab.String("prod/"),
-		})
+		}, gitlab.WithContext(ctx))
 		if err != nil {
 			return nil, fmt.Errorf("list environments: %v", err)
 		}
@@ -185,11 +268,18 @@ func getProjectID(ctx context.Context, c *gitlab.Client) (int, error) {
 	ctx, tsk := trace.NewTask(ctx, "get-project-id")
 	defer tsk.End()
 
+	ctx, span := otel.Tracer(ONAME).Start(ctx, "get-project-id")
+	defer span.End()
+
+	ctx = httptrace.WithClientTrace(ctx, otelhttptrace.NewClientTrace(ctx))
+
+	span.AddEvent("Asking gitlab for project id")
+
 	ps, r, err := c.Projects.ListProjects(&gitlab.ListProjectsOptions{
 		SearchNamespaces: gitlab.Bool(true),
 		Search:           gitlab.String(*projectFlag),
 		Visibility:       gitlab.Visibility(gitlab.PrivateVisibility),
-	})
+	}, gitlab.WithContext(ctx))
 	if err != nil {
 		return 0, fmt.Errorf("listing projects: %v", err)
 	}
